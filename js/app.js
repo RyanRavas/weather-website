5fac03fa7805389784266aa20ebb0956ce1d03d7
ryanravas <ryanaravas@gmail.com>
2024-02-29 09:11:18 -0500

    New row of weather data. Sunset and sunrise, visibility, and precipitation. Last time weather data updated.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ const apiKey = API_KEY;
 const apiUrl = ("https://api.openweathermap.org/data/2.5/weather?");
 // search form selectors
 const searchForm = document.querySelector(".search");
-const searchText = document.querySelector("#search-text-box");
 // text element selectors
 const cityNameText = document.querySelector(".city-name");
 const primaryTemperature = document.querySelector(".primary-temperature");
@@ -17,30 +16,28 @@ const windText = document.querySelector(".wind-speed-text");
 const weatherIcon = document.querySelector("#weather-icon");
 const metricButton = document.querySelector("input[value='metric']");
 const imperialButton = document.querySelector("input[value='imperial']");
+const sunriseText = document.querySelector(".sunrise-text");
+const sunsetText = document.querySelector(".sunset-text");
+const lastUpdatedText = document.querySelector(".last-updated");
+const visibilityText = document.querySelector(".visibility-text");
+const precipitationText = document.querySelector(".precipitation-text");
 // data measurement system
 let unitType = "metric";
 // longitude and latitude of location
 let longitude, latitude;
-// location var if location not in api call
-let countryString = "";
 let weatherData;
+// google place api
 let place;
 // search form submit listener
 searchForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    if (metricButton.checked) {
-        unitType = "metric";
-    } else {
-        unitType = "imperial";
-    }
     weatherData = await getWeatherData(longitude, latitude);
-    try {
-        countryString = searchText.value;
+    if (place.place_id !== undefined) {
         loadDataToDOM(weatherData, unitType, place);
-    } catch (e) {
+        addWeatherToLocalStorage(longitude, latitude, unitType);
+    } else {
         alert("Please select location using the drop down menu.");
     }
-    addWeatherToLocalStorage(longitude, latitude, unitType);
 });
 // when user clicks on a temperature unit button
 // change units for all temperature values
@@ -62,7 +59,7 @@ imperialButton.addEventListener("click", async function () {
 window.addEventListener("DOMContentLoaded", initialize);
 // loads previous city's weather if page is reset
 window.addEventListener("DOMContentLoaded", async function () {
-    // if reference does not exist, load data for toronto, ON, Canada as default
+    // if local storage reference does not exist, load data for toronto, ON, Canada as default
     if (getWeatherDataFromLocalStorage() === null) {
         // Coordinates for Toronto, ON, Canada
         longitude = -79.3839;
@@ -103,20 +100,6 @@ function initialize() {
         if (!place.geometry) {
             return;
         }
-
-        /*let address = "";
-        if (place.address_components) {
-            address = [
-                (place.address_components[0] && place.address_components[0].short_name || ''),
-                (place.address_components[1] && place.address_components[1].short_name || ''),
-                (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }*/
-        /*********************************************************************/
-        /* var address contain your autocomplete address *********************/
-        /* place.geometry.location.lat() && place.geometry.location.lat() ****/
-        /* will be used for current address latitude and longitude************/
-        /*********************************************************************/
         latitude = place.geometry.location.lat();
         longitude = place.geometry.location.lng();
     });
@@ -129,23 +112,32 @@ async function getWeatherData(longitude, latitude) {
 }
 function loadDataToDOM(weatherData, unitType, place) {
     if (unitType === "metric") {
-        primaryTemperature.innerHTML = `${formatNumber(weatherData.main.temp)}&deg;C`;
-        feelsLikeTemperature.innerHTML = `Feels Like: ${formatNumber(weatherData.main.feels_like)}&deg;C`
-        highTemperature.innerHTML = `High: ${formatNumber(weatherData.main.temp_max)}&deg;C`;
-        lowTemperature.innerHTML = `Low: ${formatNumber(weatherData.main.temp_min)}&deg;C`;
-        windText.innerHTML = `${formatNumber(weatherData.wind.speed)} km/h`;
+        primaryTemperature.innerHTML = `${formatNumber(weatherData.main.temp, 1)}&deg;C`;
+        feelsLikeTemperature.innerHTML = `Feels Like: ${formatNumber(weatherData.main.feels_like, 1)}&deg;C`
+        highTemperature.innerHTML = `High: ${formatNumber(weatherData.main.temp_max, 1)}&deg;C`;
+        lowTemperature.innerHTML = `Low: ${formatNumber(weatherData.main.temp_min, 1)}&deg;C`;
+        windText.innerHTML = `${formatNumber(weatherData.wind.speed, 1)} km/h`;
+        visibilityText.innerHTML = `${formatNumber(MToKm(weatherData.visibility), 1)}km`;
+        precipitationText.innerHTML = `${formatNumber(getPrecipitation(weatherData), 2)}mm`;
     } else {
-        primaryTemperature.innerHTML = `${formatNumber(weatherData.main.temp)}&deg;F`;
-        feelsLikeTemperature.innerHTML = `Feels Like: ${formatNumber(weatherData.main.feels_like)}&deg;F`;
-        highTemperature.innerHTML = `High: ${formatNumber(weatherData.main.temp_max)}&deg;F`;
-        lowTemperature.innerHTML = `Low: ${formatNumber(weatherData.main.temp_min)}&deg;F`;
-        windText.innerHTML = `${formatNumber(weatherData.wind.speed)} mi/h`;
+        primaryTemperature.innerHTML = `${formatNumber(weatherData.main.temp, 1)}&deg;F`;
+        feelsLikeTemperature.innerHTML = `Feels Like: ${formatNumber(weatherData.main.feels_like, 1)}&deg;F`;
+        highTemperature.innerHTML = `High: ${formatNumber(weatherData.main.temp_max, 1)}&deg;F`;
+        lowTemperature.innerHTML = `Low: ${formatNumber(weatherData.main.temp_min, 1)}&deg;F`;
+        windText.innerHTML = `${formatNumber(weatherData.wind.speed, 1)} mi/h`;
+        visibilityText.innerHTML = `${formatNumber(MToMi(weatherData.visibility), 2)}mi`;
+        precipitationText.innerHTML = `${formatNumber(MMToInches(getPrecipitation(weatherData), 2))}in`;
     }
     cityNameText.innerHTML = `${place.formatted_address}`;
     weatherTypeText.textContent = formatString(weatherData.weather[0].description);
     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`)
     humidityText.innerHTML = `${weatherData.main.humidity}%`;
+    sunsetText.innerHTML = getTime(weatherData.sys.sunset, weatherData.timezone);
+    sunriseText.innerHTML = getTime(weatherData.sys.sunrise, weatherData.timezone);
+    lastUpdatedText.innerHTML = `Data Last Updated: ${getClientTime(weatherData.dt)}`;
 }
+
+// capitalizes first letter of each word
 function formatString(string) {
     string = string.trim();
     if (string.indexOf(" ") !== -1) {
@@ -164,10 +156,25 @@ function formatString(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 }
-function formatNumber(temp) {
-    // rounds number to one decimal place
-    // if decimal is 0, doesn't show
-    return Math.round(temp * 10) / 10;
+
+// formats decimal number to specified number of decimals
+function formatNumber(temp, decimalPlaces) {
+    let multiple = 1;
+    for (let i = 0; i < decimalPlaces; i++) {
+        multiple *= 10;
+    }
+    return Math.round(temp * multiple) / multiple;
+}
+
+// formats time to xx:xx
+function formatTime(hours, minutes) {
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`
 }
 // commit required data to local storage
 function addWeatherToLocalStorage() {
@@ -182,4 +189,65 @@ function addWeatherToLocalStorage() {
 function getWeatherDataFromLocalStorage() {
     // get obj from local storage
     return localStorage.getItem("weather");
+}
+
+// takes unix time and timezone offset in seconds
+// formats to xx:xx
+function getTime(unixTime, timezoneSeconds) {
+    const dateObj = new Date((unixTime * 1000) + getRelativeOffset(timezoneSeconds));
+    return formatTime(dateObj.getHours(), dateObj.getMinutes());
+}
+
+// get timezone difference between client and weather data location
+function getRelativeOffset(timezoneSeconds) {
+    // offset in milliseconds from clients timezone
+    const clientUTCOffset = new Date(Date.now()).getTimezoneOffset() * 60000;
+    // offset in milliseconds from weather location timezone
+    const locationUTCOffset = timezoneSeconds * 1000;
+    return clientUTCOffset + locationUTCOffset;
+}
+
+// returns local time of user
+// xx:xx format
+function getClientTime(unixTime) {
+    const dateObj = new Date((unixTime * 1000) + (new Date(Date.now()).getTimezoneOffset()));
+    return formatTime(dateObj.getHours(), dateObj.getMinutes());
+}
+
+function MMToInches(mm) {
+    return mm / 25.4;
+}
+
+function MToKm(meters) {
+    return meters / 1000;
+}
+
+// returns distance in miles
+function MToMi(meters) {
+    return meters / 1609;
+}
+
+// returns precipitation in mm
+// rain or snow
+function getPrecipitation(weatherData) {
+    let rainMM, snowMM;
+    // if rain var does not exist default to undefined
+    try {
+        rainMM = weatherData.rain["1h"];
+    } catch (e) {
+        rainMM = undefined;
+    }
+    // if snow var does not exist default to undefined
+    try {
+        snowMM = weatherData.snow["1h"];
+    } catch (e) {
+        snowMM = undefined;
+    }
+    if (rainMM !== undefined) {
+        return rainMM;
+    } else if (snowMM !== undefined) {
+        return snowMM;
+    } else {
+        return 0;
+    }
 }
